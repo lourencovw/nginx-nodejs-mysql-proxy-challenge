baa1106a912e23ffb82ae36b8f170435ec51f8fb
Lourenço Viguini Werneck <lourencovw@gmail.com>
2024-05-15 21:09:06 -0300

    Update index.js

--- node/index.js
+++ node/index.js
@@ -20,18 +20,26 @@ const app = express();
 
 app.get('/', (_, res) => {
     res.set('Content-Type', 'text/html');
-    conn.query('SELECT * from people;', (err, results) => {
+    const query = 'INSERT INTO people (first_name, last_name) VALUES (?, ?)';
+    conn.query(query, ['John', 'Doe'], (err, result) => {
         if (err) {
-            console.error(err);
-            res.json({
-                success: false,
-                message: 'An error ocurred'
-            });
+            console.error('Error inserting into MySQL: ' + err.stack);
+            res.status(500).send('Error inserting into MySQL');
             return;
         }
-        const people = results.map(person => `<li>${person.first_name} ${person.last_name}</li>`).join('');
-        res.send(Buffer.from(`<h1>Full Cycle Rocks!</h1>${people}`));
-    })
+        console.log('New person added to the database with ID: ' + result.insertId);
+
+        const selectAllQuery = 'SELECT * FROM people';
+        conn.query(selectAllQuery, (err, results) => {
+            if (err) {
+                console.error('Error querying MySQL: ' + err.stack);
+                res.status(500).send('Error querying MySQL');
+                return;
+            }
+            const people = results.map(person => `<li>${person.first_name} ${person.last_name}</li>`).join('');
+            res.send(Buffer.from(`<h1>Full Cycle Rocks!</h1>${people}`));
+        });
+    });
 })
 
 app.listen(3000, () => {

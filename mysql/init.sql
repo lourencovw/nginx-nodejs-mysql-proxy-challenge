bfc598edabbbd19ba03508af0d03ef0cb0c15ee0
Lourenço Viguini Werneck <lourencovw@gmail.com>
2024-05-15 21:06:57 -0300

    Update init.sql

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,13 +5,3 @@ CREATE TABLE people (
   last_name varchar(255),
   first_name varchar(255)
 );
-INSERT INTO  db.people (first_name, last_name) VALUES  ('John', 'Doe');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('John', 'Doe');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Tom', 'Bon');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Bob', 'Rose');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Richard', 'Lennon');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Bob', 'Rose');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Richard', 'Lennon');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Jim', 'Tysone');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('John', 'Stone');
-INSERT INTO  db.people (first_name, last_name) VALUES  ('Vincent', 'Wernesk');
